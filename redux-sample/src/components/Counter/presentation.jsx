7ac4754b749f176c77395edb885c60169b610eb0
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-01-14 04:45:02 -0800

    redux-sample

--- redux-sample/src/components/Counter/presentation.jsx
+++ redux-sample/src/components/Counter/presentation.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { increment } from "../../redux/count/actions";
 
-const Counter = () => <div>counter</div>;
+const Counter = ({  count, increment, decrement }) => {
+    // console.log(props);
+
+    return(
+        <>
+        <div>{count}</div>
+        <button onClick={() => increment(1)}>+</button>
+        <button onClick={() => decrement(1)}>-</button>
+        </>
+    )
+}
 
 export default Counter;
---
+++ redux-sample/src/redux/count/actions.js
@@ -0,0 +1,12 @@
+import { DECREMENT, INCREMENT } from "./constants";
+
+export const increment = payload => ({
+
+    type: INCREMENT,
+    payload
+});
+
+export const decrement = payload => ({
+    type: DECREMENT,
+    payload
+});
